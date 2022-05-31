bb6e9e228ef9b2e78c04c312e871cbb1727ce6ab
yumi.fujimura <YUMI.FUJIMURA@grp.tmnf.jp>
2022-05-31 15:11:19 +0900

    Index/NewServletの修正

---
+++ WebContent/WEB-INF/views/events/_form.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+
+<label for="content">タスクの内容</label><br />
+<input type="text" name="content" value="${event.content}" />
+<br /><br />
+
+<input type="hidden" name="_token" value="${_token}" />
+<button type="submit">投稿</button>
--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,10 +35,15 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Event> messages = em.createNamedQuery("getAllMessages", Event.class).getResultList();
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+        List<Event> messages = em.createNamedQuery("getAllEvents", Event.class).getResultList();
 
         em.close();
+
+        request.setAttribute("messages", messages);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/events/index.jsp");
+        rd.forward(request, response);
     }
+
 
 }
